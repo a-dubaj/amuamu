0b6818b92b34159b83e76ace03cfd707177a65f9
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-14 21:47:27 +0200

    Update

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -23,7 +23,11 @@ function Popular() {
             <Wrapper>
                 <h3>Popular Picks</h3>
                 <Splide options={{
-                    perPage: 3
+                    perPage: 3,
+                    arrows: false,
+                    pagination: false,
+                    drag: "free",
+                    gap: "5rem",
                 }}>
                     {popular.map((recipe) => {
                         return (
@@ -49,10 +53,33 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
+
 
 export default Popular;
