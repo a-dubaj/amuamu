6cc06c1a5e05f064b6b94d312ae3cda872debe8e
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-15 15:47:01 +0200

    Update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -45,17 +45,29 @@ const SLink = styled(NavLink)`
   height: 6rem;
   cursor: pointer;
   transform: scale(0.8);
-  
-  h4{
+
+  h4 {
     color: white;
     font-size: 0.8rem;
   }
-  
-  svg{
+
+  svg {
     color: white;
     font-size: 1.5rem;
   }
-  
+
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg {
+      color: white;
+    }
+
+    h4 {
+      color: white;
+    }
+
+  }
 `;
 
-export default Category
+export default Category;
