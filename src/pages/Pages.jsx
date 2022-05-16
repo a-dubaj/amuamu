525a3ebc85c00230983620c1bda673e35773f5a0
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-16 13:53:48 +0200

    Update

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
-import Searched from "./Searched";
 import Recipe from "./Recipe";
 import {AnimatePresence} from "framer-motion";
 import {Route, Routes, useLocation} from "react-router-dom";
@@ -13,7 +12,6 @@ function Pages() {
             <Routes location={location} key={location.pathname}>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/cuisine/:type" element={<Cuisine/>}/>
-                <Route path="/searched/:search" element={<Searched/>}/>
                 <Route path="/recipe/:name" element={<Recipe/>}/>
             </Routes>
         </AnimatePresence>
